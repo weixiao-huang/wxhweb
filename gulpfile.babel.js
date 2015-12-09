36bf7b2d70f3d0603ea7363bf7bc0a6a96b217ba
Huang Smile <18811351753@163.com>
2015-12-08 23:10:15 -0800

    website 1.0 ok

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -70,6 +70,11 @@ gulp.task('images', () => {
     .pipe(gulp.dest('dist/images'));
 });
 
+gulp.task('videos', () => {
+  return gulp.src('app/videos/**/*')
+    .pipe(gulp.dest('dist/videos'));
+});
+
 gulp.task('fonts', () => {
   return gulp.src(require('main-bower-files')({
     filter: '**/*.{eot,svg,ttf,woff,woff2}'
@@ -106,6 +111,7 @@ gulp.task('serve', ['styles', 'fonts'], () => {
     'app/scripts/**/*.js',
     'app/styles/**/*',
     'app/images/**/*',
+    'app/videos/**/*',
     '.tmp/fonts/**/*'
   ]).on('change', reload);
 
@@ -157,7 +163,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', ['lint', 'html', 'images', 'videos', 'fonts', 'extras'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
