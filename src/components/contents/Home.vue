5dd879e0ee14e6961bcc4815d5857f017d0bb88a
huangwx <huangwx@huangwxs-MacBook-Pro.local>
2017-01-03 16:38:39 +0800

    save the current status, next is to add Projects and Researches

--- src/components/contents/Home.vue
+++ src/components/contents/Home.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
 container#home
   .section#about
-    .title Weixiao HUANG (黄维啸)
+    .bigTitle Weixiao HUANG (黄维啸)
     .subTitle Tsinghua University
     .subContent Bachelor | August, 2012 - July, 2016
     .subTitle University of California, Berkeley
@@ -12,7 +12,7 @@ container#home
       p I show a great interest and passion in computer graphics and have oriented my research focus on it, especially in real-time rendering. I've selected several related courses about it, such as Computer Graphics, Computer Graphics Real Time and Animation. I has profound mathematical foundation, nearly all grades of my mathematic course is over 90.
 
   .section#education
-    .title Education
+    .title.before Education
     .subTitle Tsinghua University
     .subContent
       p Aug. 2012 - Jul. 2016
@@ -40,46 +40,71 @@ container#home
       p.
         From October, 2015 - February, 2016, I worked at <a href="http://www.me.berkeley.edu/people/faculty/david-steigmann" target="_blank"> Prof. David Steigmann</a>'s Lab in Mechanical Engineering of <a href="http://www.berkeley.edu/" target="_blank">UC Berkeley.</a>
 
+
   .section#experiences
-    .title Research Experiences
-    .subTitle The numerical analysis of problems in the mechanics of fiber-reinforced shells
+    .title.before Research Experiences
+    //- .card
+    //-   .subTitle The numerical analysis of problems in the mechanics of fiber-reinforced shells
 
-    .subTitle
-      router-link(to="/mw") Music Workshop
-    .subContent Design Future Music
-    .content
-      p Music Workshop is a fantastic project put forward by Prof. Gangtie Zheng in 2013 and started in the summer of 2013. It was first developed by several 3-year undergraduate students majoring in Engineering Machanics in TEEP (Tsien’s Excellence in Education Program) at Tsinghua University.
-      p This program aimed to use “the magic of Mechanical Engineering” to make the instruments play music automatically, so as to explore and define “the next generation of orchestra”. Maybe you haven’t thought that thing before, but now we want to create a new band just played by machine automatically. Because of these machines, the system can play lots of difficult music which cannot be played by human beings. We believe it must be the new future of music we are imagining.
-      p From 2013 to 2015, we have got 3 creative instruments for our band: xylophone, saxophone and harp. And we have already tried a concert consisting of saxophone, harp played by the machines and piano, violin played by humans. Also, in our web, you can also get many surprises about other interesting recitals. So if you want to know what future music will be, come to see our project.
-      p Music Workshop, imagine a creative future.
+    .card
+      .subTitle Music Workshop
+      .subContent Design Future Music
+      .content
+        Music Workshop aimed to make the instruments play music automatically, so as to explore and define “the next generation of orchestra”. Now we have got 3 creative instruments for our band: xylophone, saxophone and harp. And we have already tried a concert consisting of saxophone, harp played by the machines and piano, violin played by humans.
+      .videos
+        .box(style="paddingRight: 10px")
+          iframe(src="https://www.youtube.com/embed/fBsYP7cxx_o", frameborder="0", allowfullscreen)
+        .box
+          iframe(src="https://www.youtube.com/embed/AiGWyiMEfgE", frameborder="0", allowfullscreen)
+      .vm
+        router-link(to="/mw") View More >>
 
-    .subTitle
-      router-link(to="/tunet") TUNet-pc
-    .subContent A desktop client to automatically log in and connect the university network
+    .card
+      .subTitle TUNet-pc
+      .subContent A desktop client to automatically log in and connect the university network
+      .content
+        p TUNet is a great app for students in Tsinghua University to automatically connect the campus internet conveniently as well as search useful messages of course and study by adding various package developed by Lab μ, which is a science and technology club in Tsinghua University, aiming to develop effective App to improve the convenience of students' life in Tsinghua University.
+        p This project has gained the 1st Price & Best Choice Award in Tsinghua 4th Innovation Contest, Apr. 2015
+        .imgBox
+          .img(style="paddingRight: 10px")
+            img(src="../../images/labmu/TUNet-pc.jpg")
+          .img
+            img(src="../../images/labmu/About.jpg")
+        .vm
+          router-link(to="/tunet") View More >>
 
-    .subTitle Extremely Learning Program (XLP), wind-hydro-combine power generation system
+    //- .card
+    //-   .subTitle Extremely Learning Program (XLP), wind-hydro-combine power generation system
+
+
 
   .section#projects
     .title Projects
-    .subTitle Ray Tracing - Photon Mapping
-    .content.
-      Ray Tracing is a classical method for generating a physical based rendering scene.
-    .subTitle Mesh Simplification
-    .content.
-      This project aimed to develop a algorithm to simplify a mesh object in obj files.
-    .subTitle Automatically Particle Picking
-    .content.
-      In computational biology,
-    .subTitle Spherical Gaussian Basic Function for PRT
-    .subTitle C++ Finite Element Framework
-    .subTitle OpenGL Air Hockey Game
-    .subTitle TUNet Desktop
-    .subTitle 8-Puzzle and (n^2-1)-Puzzle
+    .card
+      .subTitle Ray Tracing - Photon Mapping
+      .content.
+        Ray Tracing is a classical method for generating a physical based rendering scene.
+    .card
+      .subTitle OpenGL Air Hockey Game
+    .card
+      .subTitle Mesh Simplification
+      .content.
+        This project aimed to develop a algorithm to simplify a mesh object in obj files.
+    .card
+      .subTitle Automatically Particle Picking
+      .content.
+        In computational biology,
+    .card
+      .subTitle Spherical Gaussian Basic Function for PRT
+    .card
+      .subTitle C++ Finite Element Framework
+    .card
+      .subTitle 8-Puzzle and (n^2-1)-Puzzle
 
   .section#honors
-    .title Honors
-    .subContent American Mathematics Competition Honorable Mentions, 2016
-    .subContent 1st Price & Best Choice Award in Tsinghua 4th Innovation Contest with TUNet, Apr. 2015
+    .title.before Honors
+    .content American Mathematics Competition Honorable Mentions, 2016
+    .content 1st Price & Best Choice Award in Tsinghua 4th Innovation Contest with TUNet, Apr. 2015
 </template>
 
 <script>
@@ -128,20 +153,42 @@ table
 #home
   .section
     padding 10px 0
+
+  .card
+    border-top 1px dashed #999
+    margin-top 10px
+
+  .img
+    display inline-block
+    width 45%
+    img
+      width 100%
   a
     color #172
   .title
+    border-left 20px solid #7559aa
+    padding-left 20px
     font-size 35px
     font-weight bold
+  .bigTitle
+    font-size 35px
+    font-weight bold
+    color #123
   .subTitle
     font-size 25px
     padding-top 10px
   .content
     font-size 20px
-  #about .title
-    color #123
   .ssTitle
     font-size 20px
     font-weight bold
     padding 10px 0 5px 0
+  .box
+    display inline-block
+    iframe
+      width calc(50vw - 260px)
+      height calc(9/16 * (50vw - 260px))
+  .vm
+    text-align: center
+    font-size 20px
 </style>
--- src/components/contents/Tunet.vue
+++ src/components/contents/Tunet.vue
@@ -7,11 +7,20 @@
     .ssTitle#tunetpc TUNet-pc
     .content
       p In January, 2015, we TUNet-pc group began to develop TUNet in desktop. We mainly used Qt as the development tool and MVC design pattern to set up the framework. We were so motivated to develop it that we even worked together online during the New Year's Eve. Finally, after 2 months' work, we successfully finished a basic App which could be used to automatically connect the university network with a beautiful UI, an app that can be supported in both Windows and Max OS. Now after many improvement, we can get a better version as you can see the demo below.
+    .imgBox
+      .img(style="paddingRight: 10px")
+        img(src="../../images/labmu/TUNet-pc.jpg")
+      .img
+        img(src="../../images/labmu/About.jpg")
+    .content
       p In the future, we will add more useful functions in this App and release our product to the students in Tsinghua University, making their life more convenient by using such great App.
 
     .ssTitle#creativeComp Creative Competition
     .content
-      p In April 4, 2015, we have took part in the Tsinghua University Creative Competetion by using our TUNet learn helper package. Our lab added the learn helper plugin embedded in the TUNet Android, an App developed by another group in Lab μ, making it convenient for students to see information of their selected course and homework. For preparing the competetion, I design a fantastic video for the competetion presentation. Finally, we won the <span class="bold">Best Popular Award</span> and the <span class="bold">1st Prize of the Creative Competetion of Tsinghua University</span>
+      p In April 4, 2015, we have took part in the Tsinghua University Creative Competetion by using our TUNet learn helper package. Our lab added the learn helper plugin embedded in the TUNet Android, an App developed by another group in Lab μ, making it convenient for students to see information of their selected course and homework. For preparing the competetion, I design a fantastic video for the competetion presentation. Finally, we won the <span class="bold">Best Choice Award</span> and <span class="bold">the 1st Prize of the Creative Competetion of Tsinghua University</span>
+    img(style="padding: 20px 0", width="100%", src="../../images/labmu/CreativeComp.jpg")
+    iframe(src="https://www.youtube.com/embed/NOwgt2Hflsk", frameborder="0", allowfullscreen)
+
 
 </template>
 
@@ -27,21 +36,13 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-#mw
-  a
-    color #172
-  .title
-    font-size 35px
-    font-weight bold
-  .subTitle
-    font-size 25px
-    padding-top 10px
-  .content
-    font-size 20px
-  #about .title
-    color #123
-  .ssTitle
-    font-size 20px
-    font-weight bold
-    padding 10px 0 5px 0
+#tunet
+  .img
+    display inline-block
+    width 48%
+    img
+      width 100%
+  iframe
+    width calc(100vw - 500px)
+    height calc(9/16 * (100vw - 500px))
 </style>
